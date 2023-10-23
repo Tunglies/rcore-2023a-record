7bbf457c51dc2c5f6d793e1c528ad309a3f28782
Tunglies <tunglies@outlook.com>
2023-10-23 23:47:46 +0800

    Mon Oct 23 23:47:46 CST 2023

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# rcore-2023a-record
-
 2023年秋季rcore训练营记录
 
-[toc]
+# rcore-2023a-record
+
+[TOC]
 
 ## [2023-10-23](docs/2023-10-23.md)
--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 > README.md
-echo "# rcore-2023a-record" >> README.md
-echo "" >> README.md
 echo "2023年秋季rcore训练营记录" >> README.md
 echo "" >> README.md
-echo "[toc]" >> README.md
+echo "# rcore-2023a-record" >> README.md
+echo "" >> README.md
+echo "[TOC]" >> README.md
 echo "" >> README.md
 
 for doc in $(ls docs)
